a874a23795a0b228367a414edc181b2844aefcc4
Atul Singh <atulsingh@microsoft.com>
2023-08-06 12:30:56 +0530

    Initial Commit

---
+++ color_difference_app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+def rgb_to_xyz(r, g, b):
+    # Converts RGB to CIE 1931 XYZ color space
+    r = r / 255.0
+    g = g / 255.0
+    b = b / 255.0
+
+    if r > 0.04045:
+        r = ((r + 0.055) / 1.055) ** 2.4
+    else:
+        r = r / 12.92
+
+    if g > 0.04045:
+        g = ((g + 0.055) / 1.055) ** 2.4
+    else:
+        g = g / 12.92
+
+    if b > 0.04045:
+        b = ((b + 0.055) / 1.055) ** 2.4
+    else:
+        b = b / 12.92
+
+    r = r * 100.0
+    g = g * 100.0
+    b = b * 100.0
+
+    # D65 illuminant
+    x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
+    y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
+    z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
+
+    return x, y, z
+
+def delta_e_cie1976(x1, y1, z1, x2, y2, z2):
+    # Computes the CIE 1976 Delta E color difference
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5
+
+def main():
+    st.title("Color Difference Calculator")
+
+    st.write("Enter the RGB values of the two colors below:")
+
+    r1 = st.slider("Color 1 - Red", 0, 255, 128)
+    g1 = st.slider("Color 1 - Green", 0, 255, 128)
+    b1 = st.slider("Color 1 - Blue", 0, 255, 128)
+
+    r2 = st.slider("Color 2 - Red", 0, 255, 128)
+    g2 = st.slider("Color 2 - Green", 0, 255, 128)
+    b2 = st.slider("Color 2 - Blue", 0, 255, 128)
+
+    x1, y1, z1 = rgb_to_xyz(r1, g1, b1)
+    x2, y2, z2 = rgb_to_xyz(r2, g2, b2)
+
+    delta_e = delta_e_cie1976(x1, y1, z1, x2, y2, z2)
+
+    st.write(f"The CIE 1976 Delta E color difference is: {delta_e:.2f}")
+
+if __name__ == "__main__":
+    main()
